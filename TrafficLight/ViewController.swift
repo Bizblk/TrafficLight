8a19d7466f386bc49e2730c45894c445ed59bacb
Bizblk <enemy_counter@mail.ru>
2021-03-24 18:49:52 +0300

    end work

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,48 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var yellowView: UIView!
     @IBOutlet weak var greenView: UIView!
     @IBOutlet weak var startButton: UIButton!
     
+    // MARK: - Private Properties
+    private let dark: CGFloat = 0.3
+    private let light: CGFloat = 1
     
-    
-       override func viewWillLayoutSubviews() {
-           super.viewWillLayoutSubviews()
-        redView.layer.cornerRadius = redView.frame.size.width / 2
-        yellowView.layer.cornerRadius = yellowView.frame.size.width / 2
-        greenView.layer.cornerRadius = greenView.frame.size.width / 2
-
-    //    redView.clipsToBounds = true
-}
+    // MARK: - Life Cyles Methods
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redView.alpha = 0.3
-     //   redView.layer.cornerRadius = redView.frame.size.width / 2
-        //redView.clipsToBounds = true
-        yellowView.alpha = 0.3
-      //  yellowView.layer.cornerRadius = 50
-        greenView.alpha = 0.3
-      //  greenView.layer.cornerRadius = 50
+        redView.alpha = dark
+        yellowView.alpha = dark
+        greenView.alpha = dark
         startButton.layer.cornerRadius = 15
-        // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - IB Actions
     @IBAction func actionButton(_ sender: UIButton) {
         
-        if redView.alpha == yellowView.alpha {
-            redView.alpha = 1
-            greenView.alpha = 0.3
+        if greenView.alpha == light {
+            startButton.setTitle("Start", for: .normal)
+            greenView.alpha = dark
+        } else if redView.alpha == yellowView.alpha {
+            redView.alpha = light
+            greenView.alpha = dark
             startButton.setTitle("Red", for: .normal)
-        } else if redView.alpha == 1 {
-            yellowView.alpha = 1
-            redView.alpha = 0.3
+        } else if redView.alpha == light {
+            yellowView.alpha = light
+            redView.alpha = dark
             startButton.setTitle("Yellow", for: .normal)
-        } else if yellowView.alpha == 1 {
-            greenView.alpha = 1
-            yellowView.alpha = 0.3
+        } else if yellowView.alpha == light {
+            greenView.alpha = light
+            yellowView.alpha = dark
             startButton.setTitle("Green", for: .normal)
         }
     }
